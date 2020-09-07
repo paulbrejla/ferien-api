1e1a0438327d347a07c20d88d2555b3862b087a4
paul <paul.brejla@googlemail.com>
2020-09-07 23:41:23 +0200

    allow json param

--- src/main/kotlin/de/paulbrejla/holidays/rest/api/HolidayWsV1.kt
+++ src/main/kotlin/de/paulbrejla/holidays/rest/api/HolidayWsV1.kt
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod
 @RequestMapping("/api/v1", produces = ["application/json"])
 interface HolidayWsV1 {
 
-    @RequestMapping("/holidays", method = [RequestMethod.GET])
+    @RequestMapping("/holidays", "/holidays.json", method = [RequestMethod.GET])
     fun getHolidays(): List<HolidayDto>
 
-    @RequestMapping("/holidays/{state}", method = [RequestMethod.GET])
+    @RequestMapping("/holidays/{state}.json", "/holidays/{state}", method = [RequestMethod.GET])
     fun getHolidaysForState(@PathVariable("state") state: State): List<HolidayDto>
 
-    @RequestMapping("/holidays/{state}/{year}", method = [RequestMethod.GET])
+    @RequestMapping("/holidays/{state}/{year}.json", "/holidays/{state}/{year}", method = [RequestMethod.GET])
     fun getHolidaysForStateAndYear(@PathVariable("state") state: State, @PathVariable("year") year: Int): List<HolidayDto>
 
 }
