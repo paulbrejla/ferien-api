ae57541c4234e52d4f47815c8707ffb08114656f
paul <paul.brejla@googlemail.com>
2020-09-07 18:44:47 +0200

    cleanup

--- src/main/kotlin/de/paulbrejla/holidays/rest/impl/HolidayWsV1Impl.kt
+++ src/main/kotlin/de/paulbrejla/holidays/rest/impl/HolidayWsV1Impl.kt
@@ -4,7 +4,6 @@ import de.paulbrejla.holidays.application.api.HolidayService
 import de.paulbrejla.holidays.domain.State
 import de.paulbrejla.holidays.rest.HolidayDto
 import de.paulbrejla.holidays.rest.api.HolidayWsV1
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException
 import java.lang.Exception
 
 @RestController
-class HolidayWsV1Impl @Autowired constructor(val holidayService: HolidayService) : HolidayWsV1 {
+class HolidayWsV1Impl(val holidayService: HolidayService) : HolidayWsV1 {
 
     override fun getHolidaysForState(@PathVariable("state") state: State): List<HolidayDto> {
         return holidayService.findHolidays(forState = state)
