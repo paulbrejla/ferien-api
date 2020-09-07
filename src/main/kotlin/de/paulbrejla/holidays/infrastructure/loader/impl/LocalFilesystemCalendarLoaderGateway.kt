83feb76be44da7accb452caa13e6e2a66b5ae59a
paul <paul.brejla@googlemail.com>
2020-09-07 10:15:29 +0200

    readme

--- src/main/kotlin/de/paulbrejla/holidays/infrastructure/loader/impl/LocalFilesystemCalendarLoaderGateway.kt
+++ src/main/kotlin/de/paulbrejla/holidays/infrastructure/loader/impl/LocalFilesystemCalendarLoaderGateway.kt
@@ -3,18 +3,10 @@ package de.paulbrejla.holidays.infrastructure.loader.impl
 import de.paulbrejla.holidays.infrastructure.loader.api.CalendarLoader
 import biweekly.Biweekly
 import de.paulbrejla.holidays.infrastructure.CalendarDto
-import de.paulbrejla.holidays.infrastructure.assembleIcsFileNames
-import de.paulbrejla.holidays.infrastructure.assembleIcsString
 import de.paulbrejla.holidays.infrastructure.assembleStateFromFileName
-import org.apache.commons.io.IOUtils
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
-import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
-import kotlin.streams.toList
 
 
 @Component
