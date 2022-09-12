74ccda7d8a2c79342d7aebc7deaad590570769f4
Paul Brejla <paul.brejla@googlemail.com>
2022-09-12 21:40:39 +0200

    use proper json format

--- src/main/kotlin/de/paulbrejla/holidays/rest/Assembler.kt
+++ src/main/kotlin/de/paulbrejla/holidays/rest/Assembler.kt
@@ -3,8 +3,8 @@ package de.paulbrejla.holidays.rest
 import de.paulbrejla.holidays.domain.Holiday
 
 fun assembleHolidayDto(holiday: Holiday) = HolidayDto(
-        start = holiday.start.toString(),
-        end = holiday.end.toString(),
+        start = holiday.start,
+        end = holiday.end,
         year = holiday.year,
         stateCode = holiday.stateCode,
         name = holiday.summary,
--- src/main/kotlin/de/paulbrejla/holidays/rest/Dto.kt
+++ src/main/kotlin/de/paulbrejla/holidays/rest/Dto.kt
@@ -1,6 +1,14 @@
 package de.paulbrejla.holidays.rest
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import de.paulbrejla.holidays.domain.State
 import java.time.LocalDate
 
-data class HolidayDto(val start: String, val end: String, val year: Int, val stateCode: State, val name: String, val slug: String)
+data class HolidayDto(
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") val start: LocalDate,
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") val end: LocalDate,
+    val year: Int,
+    val stateCode: State,
+    val name: String,
+    val slug: String
+)
